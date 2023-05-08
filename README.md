7206936fe9869fb4a25ea76729bb0b0d62818290
SerKo <serko.dev@gmail.com>
2023-05-09 01:38:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# unocss-preset-colors-rgb
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import type { Preset } from '@unocss/core'
+
+function convertColorsToRgb(colors: Record<string, any>): Record<string, any> {
+  if (typeof colors !== 'object')
+    return {}
+
+  const convertedColors: Record<string, any> = {}
+  for (const key in colors) {
+    if (typeof colors[key] === 'string') {
+      const rgb = hexToRgb(colors[key])
+      if (rgb)
+        convertedColors[key] = rgb
+    } else if (typeof colors[key] === 'object') {
+      convertedColors[key] = convertColorsToRgb(colors[key])
+    }
+  }
+  return convertedColors
+}
+
+function hexToRgb(hex: string): string | undefined {
+  // Remove the hash character if it exists
+  hex = hex.replace('#', '')
+
+  if (hex.length !== 3 && hex.length !== 6)
+    return
+
+  // Expand short form (3 digits) to long form (6 digits)
+  hex = hex.length === 3
+    ? hex.split('').map(digit => digit + digit).join('')
+    : hex
+
+  // Convert the hex string to a number
+  const num = parseInt(hex, 16)
+  if (isNaN(num))
+    return
+
+  // Extract the red, green, and blue components
+  const red = (num >> 16) & 255
+  const green = (num >> 8) & 255
+  const blue = num & 255
+
+  // Return the RGB value as a string
+  return `${red}, ${green}, ${blue}`
+}
+
+export default function presetColorsRGB(colors?: Record<string, any>): Preset {
+  return {
+    name: 'unocss-preset-colors-rgb',
+    extendTheme: (theme: Record<string, any>) => {
+      theme.rgbs = colors ?? convertColorsToRgb(theme.colors)
+      return theme
+    },
+  }
+}
